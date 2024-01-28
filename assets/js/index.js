1ab2001685c1282c69c89d50525445f0542e13cd
White Lotus Developers <157888768+whitelotusdevelopers@users.noreply.github.com>
2024-01-28 12:23:36 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,7 +10,7 @@
   });
 
   menu.addEventListener("click", function (e) {
-    if (navbar.contains(e.target)) {
+    if (!navbar.contains(e.target)) {
       this.classList.remove("active");
       menu.classList.remove("active");
     }
@@ -20,5 +20,11 @@
     btn.addEventListener("click", function () {
       this.classList.toggle("active");
     });
+  });
+
+  document..addEventListener("click", function (e) {
+    if (dropdown.forEach(btn => !btn.contains(e.target))) {
+      dropdown.forEach(btn => !btn..classList.remove("active"));
+    }
   });
 })();
