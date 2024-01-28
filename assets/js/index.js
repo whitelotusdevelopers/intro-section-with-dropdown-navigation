80a7eb2de35318940ba908968f12d8dfa09e08e7
White Lotus Developers <157888768+whitelotusdevelopers@users.noreply.github.com>
2024-01-28 13:24:37 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,12 +6,17 @@
   
   toggle.addEventListener("click", function () {
     this.classList.toggle("active");
+    if (toggle.ariaExpanded == "true") {
+      toggle.ariaExpanded = false;
+    } else toggle.ariaExpanded = true;
     menu.classList.toggle("active");
+    console.log(toggle.ariaExpanded);
   });
 
   menu.addEventListener("click", function (e) {
-    if (!navbar.contains(e.target)) {
-      this.classList.remove("active");
+    if (!navbar.contains(e.target) && !toggle.contains(e.target)) {
+      toggle.classList.remove("active");
+      toggle.ariaExpanded = false;
       menu.classList.remove("active");
     }
   });
@@ -24,8 +29,7 @@
 
   document.addEventListener("click", function (e) {
     dropdown.forEach(btn => {
-      if (!btn.contains(e.target) && !toggle.contains(e.target)) {
-        toggle.classList.remove("active");
+      if (!btn.contains(e.target)) {
         btn.parentElement.classList.remove("active");
       }
     });
