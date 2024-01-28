1823347fab6054e0e05aa3d4dca132cb3722e15f
White Lotus Developers <157888768+whitelotusdevelopers@users.noreply.github.com>
2024-01-28 12:13:56 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,19 @@
 (function () {
   const toggle = document.getElementById("toggle"),
     menu = document.getElementById("menu"),
+    navbar = document.getElementById("navbar"),
     dropdown = document.querySelectorAll(".dropdown");
   
   toggle.addEventListener("click", function () {
     this.classList.toggle("active");
     menu.classList.toggle("active");
+  });
+
+  menu.addEventListener("click", function (e) {
+    if (navbar.contains(e.target)) {
+      this.classList.remove("active");
+      menu.classList.remove("active");
+    }
   });
 
   dropdown.forEach(btn => {
